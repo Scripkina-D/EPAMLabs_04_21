8f836f16247eb532b5f3a36a6486a5a9dcd9430a
Scripkina-D <scripkina.daria@yandex.ru>
2021-04-29 18:20:41 +0300

    End HW

---
+++ Lab-1/lab1_2/lab1_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace lab1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            double a1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите второе число: ");
+            double a2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Выберите действие : + ,  - ,  * , /  \n");
+            string b1 = Console.ReadLine();
+            switch (b1)
+
+            {
+                case "+":
+                    Console.WriteLine($"Ответ: {a1 + a2} ");
+                    break;
+                case "-":
+                    Console.WriteLine($"Ответ: {a1 - a2} ");
+                    break;
+                case "*":
+                    Console.WriteLine($"Ответ: {a1 * a2} ");
+                    break;
+                case "/":
+                    Console.WriteLine($"Ответ: {a1 / a2} ");
+                    break;
+                default:
+                    Console.WriteLine("Вы выбрали неизвестное действие");
+                    break;
+            }
+         }
+    }
+}
