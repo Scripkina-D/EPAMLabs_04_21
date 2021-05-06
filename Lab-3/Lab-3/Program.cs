6c1f0024eed21eb8c20f256fc332422a555df812
Scripkina-D <scripkina.daria@yandex.ru>
2021-05-06 17:24:41 +0300

    End HW

---
+++ Lab-3/Lab-3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Musician> x = new List<Musician>();
+
+            x.Add(new Musician() { Name = "Филипп Киркоров", NumberSongs = 23 });
+            x.Add(new Musician() { Name = "Клава Кока", NumberSongs = 14 });
+            x.Add(new Musician() { Name = "Земфира", NumberSongs = 18 });
+            x.Add(new Musician() { Name = "Дима Билан", NumberSongs = 72 });
+            x.Add(new Musician() { Name = "Сергей Лазарев", NumberSongs = 34 });
+
+            List<Musician> x1 = x.Where(t => t.NumberSongs > 20).ToList();
+
+
+            Console.WriteLine("Исполнители выпустившие более 20 песен");
+            foreach (Musician m in x1)
+            {
+                Console.WriteLine($"{m.Name} выпустил {m.NumberSongs} песен");
+            }
+
+        }
+
+
+        class Musician
+        {
+            public int NumberSongs { get; set; }
+
+            public string Name { get; set; }
+
+
+        }
+      
+    }
+}
